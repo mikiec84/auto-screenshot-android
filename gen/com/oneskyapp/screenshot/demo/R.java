4b8eea17b6a4b7ea4f25d035ccab1857a4a1ac87
Bret Cheng <bretdabaker@gmail.com>
2014-02-04 15:20:24 +0800

    Fix regex pattern bug

--- gen/com/oneskyapp/screenshot/demo/R.java
+++ gen/com/oneskyapp/screenshot/demo/R.java
@@ -45,10 +45,11 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int array=0x7f050007;
+        public static final int array=0x7f050008;
         public static final int button=0x7f050004;
         public static final int edit_text=0x7f050005;
         public static final int hello_world=0x7f050002;
+        public static final int regex=0x7f050007;
         public static final int test_format=0x7f050006;
         public static final int text_view=0x7f050003;
     }
